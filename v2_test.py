621f74ece8dd7006e1cee3b65ad9f293d357a80b
John Olusetire <johnolusetire97@gmail.com>
2025-07-09 19:48:25 -0400

    Updated test files

--- v2_test.py
+++ v2_test.py
@@ -1,13 +1,37 @@
 #from cs336_basics.bpe import train_bpe
 from cs336_basics.bpe_v6 import train_bpe
+from cs336_basics.damekbpe import BPETrainer
 import time
 
 #path = "tests/fixtures/corpus.en"
-path = "tests/fixtures/tinystories_sample_5M.txt"
+#path = "tests/fixtures/tinystories_sample_5M.txt"
+path = "data/TinyStoriesV2-GPT4-valid.txt"
+vocab_size = 10000
+special_tokens = ["<|endoftext|>"]
+
+
 start_time = time.time()
-vocab, merges = train_bpe(path, 1000, ["<|endoftext|>"])
+vocab1, merges1 = train_bpe(path, vocab_size, special_tokens)
 end_time = time.time()
-print(f"It took {end_time-start_time} to train")
+print(f"It took {end_time-start_time} to train old")
+print("-" * 50)
+
+# start_time = time.time()
+# bpe = BPETrainer()
+# vocab2, merges2 = bpe.train(path, vocab_size, special_tokens)
+# end_time = time.time()
+# print(f"It took {end_time-start_time} to train new")
+# print("-" * 50)
+
+# if vocab1 == vocab2:
+#     print("vocabs match")
+# else:
+#     print("vocabs don't match")
+
+# if merges1 == merges2:
+#     print("merges match")
+# else:
+#     print("merges don't match")
 
 # import pickle
 # save_path = "v2.pkl"
