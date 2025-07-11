724342b5311cc04529733570b82bf5a84ddfb214
John Olusetire <johnolusetire97@gmail.com>
2025-07-10 23:51:50 -0400

    Update version in max_bpe.ipynb to 3.13.2 and adjust vocab_size in v2_test.py to 10000 with improved training output

--- v2_test.py
+++ v2_test.py
@@ -7,32 +7,33 @@ import time
 #path = "tests/fixtures/corpus.en"
 #path = "tests/fixtures/tinystories_sample_5M.txt"
 path = "data/TinyStoriesV2-GPT4-train.txt"
-vocab_size = 10
+vocab_size = 10000
 special_tokens = ["<|endoftext|>"]
 
 
-# start_time = time.time()
-# vocab1, merges1 = train_bpe(path, vocab_size, special_tokens, mode="multi", verbose=True)
-# end_time = time.time()
-# print(f"It took {end_time-start_time} to train old")
-# print("-" * 50)
+start_time = time.time()
+vocab1, merges1 = train_bpe(path, vocab_size, special_tokens, mode="multi", verbose=False)
+end_time = time.time()
+print(f"It took {end_time-start_time} to train")
+print("-" * 50)
+print(f"Vocab length: {len(vocab1)}")
+print(f"Merge length: {len(merges1)}")
 
 start_time = time.time()
 bpe = BPETrainer()
-vocab2, merges2 = bpe.train(path, vocab_size, special_tokens)
+vocab2, merges2 = bpe.train(path, vocab_size, special_tokens, verbose=False)
 end_time = time.time()
 print(f"It took {end_time-start_time} to train new")
 print("-" * 50)
 
-# if vocab1 == vocab2:
-#     print("vocabs match")
-# else:
-#     print("vocabs don't match")
+for i in range(256,len(vocab1)):
+    if vocab1[i] != vocab2[i]:
+        print(f"{vocab1[i]}     {vocab2[i]}")
 
-# if merges1 == merges2:
-#     print("merges match")
-# else:
-#     print("merges don't match")
+if merges1 == merges2:
+    print("merges match")
+else:
+    print("merges don't match")
 
 # import pickle
 # save_path = "v2.pkl"
